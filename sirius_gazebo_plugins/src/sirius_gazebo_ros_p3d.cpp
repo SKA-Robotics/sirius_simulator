5bae3c5248ea3e12902be0c3074a5212312a28b7
Gabriel Brzezinski <gabriel@gabrielb.dev>
2024-01-27 21:40:56 +0100

    fix(sirius_gazebo_plugins): report twist ground truth in body frame

--- sirius_gazebo_plugins/src/sirius_gazebo_ros_p3d.cpp
+++ sirius_gazebo_plugins/src/sirius_gazebo_ros_p3d.cpp
@@ -312,6 +312,12 @@ void SiriusGazeboRosP3D::UpdateChild()
                     vpos = frame_pose.Rot().RotateVector(vpos - frame_vpos);
                     veul = frame_pose.Rot().RotateVector(veul - frame_veul);
                 }
+                else
+                {
+                    const auto rot_inv = pose.Rot().Inverse();
+                    vpos = rot_inv.RotateVector(vpos);
+                    veul = rot_inv.RotateVector(veul);
+                }
 
                 // Apply Constant Offsets
                 // apply xyz offsets and get position and rotation components
